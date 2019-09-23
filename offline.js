095da082d8b88dc75e5bf2a6bcedb3b03ad4a99f
61131114002 <43134332+61131114002@users.noreply.github.com>
2019-09-23 21:33:42 +0700

    Map2

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1569249142,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/objuct-sheet0.png",
+		"images/objuct-sheet1.png",
+		"images/objuct-sheet2.png",
+		"images/pum-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
